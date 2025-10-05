ea5fd64e1e9870dbe1bde682e8534c4dad16c447
immun1ty <so_kaicyo@icloud.com>
2025-10-06 07:09:55 +0900

    update monthly file

--- scripts/monthly.py
+++ scripts/monthly.py
@@ -57,7 +57,9 @@ def update_readme(qiita, zenn, github):
 ## GitHub Trending (Top {len(github)})
 """ + "\n".join([f"- [{x['title']}]({x['url']})\n  - {x['desc']}" for x in github]) + """
 """
-    with open("MONTHLY.md", "w", encoding="utf-8") as f:
+    output_dir_docs = Path("docs")
+    output_dir.mkdir(parents=True, exist_ok=True)
+    with open(out_dir_docs / "MONTHLY.md", "w", encoding="utf-8") as f:
         f.write(content)
 
 if __name__ == "__main__":
